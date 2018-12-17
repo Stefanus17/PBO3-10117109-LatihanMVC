dbd7c0985edc1de245d69c24bf81f371a9e28508
Stefanus17 <43497735+Stefanus17@users.noreply.github.com>
2018-12-17 22:41:12 +0700

    Add files via upload

---
+++ LatihanMVC/src/edu/stefanus/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,21 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stefanus.latihanmvc.event;
+
+import edu.stefanus.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author Asus
+ * Nama  : Stefanus Saputra
+ * NIM   : 10117109
+ * Kelas : IF3
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+    
+}
